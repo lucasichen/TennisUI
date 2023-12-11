2f4614c9adc3966de8342bb790819163fc4b3d7f
Lucas Chen <lucas.chen2016@gmail.com>
2023-12-11 08:24:01 -0500

    fix: fix login with auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,39 @@
 // App.js
-import React, { useEffect } from 'react';
+import React from 'react';
 import ScheduleTabs from './pages/ScheduleTabs';
 import { Box } from '@mui/material';
 import { SnackbarProvider } from 'notistack';
 import { useAuth0 } from '@auth0/auth0-react';
-import { CircularProgress } from '@mui/material';
 
 function App() {
-  const { isAuthenticated, loginWithRedirect, isLoading, error } = useAuth0();
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      loginWithRedirect();
-    }
-  }, [isAuthenticated]);
-
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   if (error) {
     return <div>Oops... {error.message}</div>;
   }
 
-  if (isLoading) {
-    return <CircularProgress />;
+  if (isAuthenticated) {
+    return (
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+      >
+        <Box style={{ background: '#faf0e6' }} height='100vh' width="100%">
+          <button onClick={() => logout({ returnTo: window.location.origin })}> Log out </button>
+          <ScheduleTabs />
+        </Box>
+      </SnackbarProvider>
+    )
+  } else {
+    return <button onClick={loginWithRedirect}>Log in</button>;
   }
-
-  return (
-    <SnackbarProvider
-      maxSnack={3}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-    >
-      <Box style={{ background: '#faf0e6' }} height='100vh' width="100%">
-        <ScheduleTabs />
-      </Box>
-    </SnackbarProvider>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from "@auth0/auth0-react";
-import { useAuth0 } from "@auth0/auth0-react";
-import history from "./utils/history";
-import { getConfig } from "./config";
-
-const onRedirectCallback = (appState) => {
-  history.push(
-    appState && appState.returnTo ? appState.returnTo : window.location.pathname
-  );
-};
-
-// Please see https://auth0.github.io/auth0-react/interfaces/Auth0ProviderOptions.html
-// for a full list of the available properties on the provider
-const config = getConfig();
-
-const providerConfig = {
-  domain: config.domain,
-  clientId: config.clientId,
-  onRedirectCallback,
-  authorizationParams: {
-    redirect_uri: window.location.origin,
-    ...(config.audience ? { audience: config.audience } : null),
-  },
-};
-
+import { getConfig } from './config';
+import { Auth0Provider } from '@auth0/auth0-react';
+import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const config = getConfig();
 
 root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      {...providerConfig}
-    >
-      <App />
-    </Auth0Provider>,
-  </React.StrictMode>
+  <Auth0Provider
+    domain={config.domain}
+    clientId={config.clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
